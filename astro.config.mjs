1367f384b83e654906cccb2d016fe73ff8cae287
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-21 21:55:17 +0300

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nh.naai.nz',
-    output: 'server'
+  site: 'https://nh.naai.nz',
+  output: 'server',
+  adapter: vercel()
 });
